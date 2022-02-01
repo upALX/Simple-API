09b1e1f48bdbf8eb2bc591d769c33ad21c1fed55
Alexandre <alexandreaddres@gmail.com>
2022-02-01 15:49:09 -0300

    Add all endpoints of API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 import pandas as pd
 
 app = Flask(__name__)
@@ -6,15 +6,28 @@ app = Flask(__name__)
 tableReaded = pd.read_csv('API-DATA.csv')
 
 @app.route('/')
-def singlePage():
+def index():
+    welc = 'Welcome to simple API (this is not endpoint)'
+    return welc
+
+@app.route('/dataSales')
+def dataSales():
     valuesSales = tableReaded['Vendas'].sum()
+    return jsonify('Vendas {}'.format(valuesSales))
+
+@app.route('/dataTV')
+def dataTV():
     valuesTV = tableReaded['TV'].sum()
+    return jsonify('TV {}'.format(valuesTV))
+
+@app.route('/dataRadio')
+def dataRadio():
     valuesRadio = tableReaded['Radio'].sum()
+    return jsonify('Radio {}'.format(valuesRadio))
+
+@app.route('/dataJornal')
+def dataJournal():
     valuesJornal = tableReaded['Jornal'].sum()
-    return jsonify(
-            'TV {}'.format(valuesTV),
-            'Radio {}'.format(valuesRadio),
-            'Jornal {}'.format(valuesJornal),
-            'Vendas {}'.format(valuesSales))
+    return jsonify('Jornal {}'.format(valuesJornal))
 
 app.run()
